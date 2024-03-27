e0171200030f6a2588f4fd0835c16576e57c62c0
Suraj Kumar <kumarsuraj345678@gmail.com>
2024-03-27 13:44:49 +0530

    projects added

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { getImageUrl } from "../../utils";
 import styles from "./ProjectCard.module.css";
+
 const ProjectCard = ({
   project: { title, imageSrc, description, skills, demo, source },
 }) => {
+  const backgroundImageStyle = {
+    backgroundImage: `url(${getImageUrl(imageSrc)})`,
+  };
+
   return (
     <div className={styles.container}>
-      <img
-        className={styles.image}
-        src={getImageUrl(imageSrc)}
-        alt={`Image of ${title}`}
-      />
+      <div className={styles.image} style={backgroundImageStyle}></div>
       <h3 className={styles.title}>{title}</h3>
       <p className={styles.description}>{description}</p>
       <ul className={styles.skills}>
