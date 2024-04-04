d1ae487d4e7d6a4e2f2defb27313e4276db6ccc8
Suraj Kumar <kumarsuraj345678@gmail.com>
2024-04-04 11:06:34 +0530

    Refactored openResume component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ import { getImageUrl } from "../../utils";
 export const Hero = () => {
   const openResume = () => {
     window.open(
-      "https://drive.google.com/file/d/15OW_2A2BFwgWPPL2RFtQj7oUTzMhlrtu/view?usp=sharing",
+      "https://drive.google.com/file/d/1NBEOTqH0SX1oM-vgsf7qOy9G-ctxn8S7/view?usp=drive_link",
       "_blank",
       "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=100,width=800,height=600"
     );
